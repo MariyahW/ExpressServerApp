3a1de04b32bc4a7c6f799485b635d68a2d7512b7
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-01-23 20:31:19 -0500

    Updated data sets

---
+++ data/base.js
@@ -0,0 +1,88 @@
+const base = {
+    "id" : 571561442,
+    "name" : "Base Salary",
+    "dataTypeName" : "number",
+    "description" : "Annual base salary for the employee at the end of the calendar year. NOTE: This is the projected salary based on employee status and hours the employee is scheduled to work.",
+    "fieldName" : "base_salary",
+    "position" : 5,
+    "renderTypeName" : "number",
+    "tableColumnId" : 145675083,
+    "width" : 152,
+    "cachedContents" : {
+      "non_null" : "10052",
+      "largest" : "275000.00",
+      "null" : "0",
+      "top" : [ {
+        "item" : "100900.00",
+        "count" : "388"
+      }, {
+        "item" : "93895.00",
+        "count" : "232"
+      }, {
+        "item" : "113091.00",
+        "count" : "225"
+      }, {
+        "item" : "107934.00",
+        "count" : "224"
+      }, {
+        "item" : "103034.00",
+        "count" : "224"
+      }, {
+        "item" : "74614.00",
+        "count" : "202"
+      }, {
+        "item" : "102180.00",
+        "count" : "147"
+      }, {
+        "item" : "81770.00",
+        "count" : "147"
+      }, {
+        "item" : "116536.00",
+        "count" : "138"
+      }, {
+        "item" : "92895.00",
+        "count" : "137"
+      }, {
+        "item" : "126754.00",
+        "count" : "136"
+      }, {
+        "item" : "85615.00",
+        "count" : "135"
+      }, {
+        "item" : "89653.00",
+        "count" : "124"
+      }, {
+        "item" : "77684.00",
+        "count" : "117"
+      }, {
+        "item" : "130167.00",
+        "count" : "114"
+      }, {
+        "item" : "118509.00",
+        "count" : "103"
+      }, {
+        "item" : "65109.00",
+        "count" : "83"
+      }, {
+        "item" : "63684.00",
+        "count" : "80"
+      }, {
+        "item" : "112408.00",
+        "count" : "76"
+      }, {
+        "item" : "148607.00",
+        "count" : "71"
+      } ],
+      "smallest" : "11147.24",
+      "count" : "10052",
+      "cardinality" : "3239"
+    },
+    "format" : {
+      "precisionStyle" : "currency",
+      "decimalSeparator" : ".",
+      "currencyStyle" : "USD",
+      "groupSeparator" : ","
+    }
+  }
+
+  module.exports= base;
---
+++ data/deptartment.js
@@ -0,0 +1,110 @@
+const department = {
+    "id" : 571561446,
+    "name" : "Department",
+    "dataTypeName" : "text",
+    "description" : "Department code for the department to which the employee is assigned.",
+    "fieldName" : "department",
+    "position" : 1,
+    "renderTypeName" : "text",
+    "tableColumnId" : 145675087,
+    "width" : 122,
+    "cachedContents" : {
+      "non_null" : "10052",
+      "largest" : "ZAH",
+      "null" : "0",
+      "data" : [ {
+        "name": "Sheriff's Office",
+        "item" : "ZAH",
+        },
+         {
+        "name": "Department of Police",
+        "item" : "POL",
+        "count" : "1858"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "HHS",
+        "count" : "1797"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "FRS",
+        "count" : "1396"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "DOT",
+        "count" : "1277"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "COR",
+        "count" : "511"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "ABS",
+        "count" : "445"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "DGS",
+        "count" : "421"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "LIB",
+        "count" : "384"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "DPS",
+        "count" : "230"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "SHF",
+        "count" : "201"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "DEP",
+        "count" : "172"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "REC",
+        "count" : "171"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "TBS",
+        "count" : "147"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "FIN",
+        "count" : "121"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "CCL",
+        "count" : "121"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "HCA",
+        "count" : "101"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "CEC",
+        "count" : "100"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "CAT",
+        "count" : "77"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "OAS",
+        "count" : "75"
+      }, {
+        "name": "Department of Health and Human Services",
+        "item" : "OHR",
+        "count" : "69"
+      },
+      {
+        "name": "Alcohol Beverage Services",
+        "item" : "ABS",
+        } ],
+      "smallest" : "ABS",
+      "count" : "10052",
+      "cardinality" : "41"
+    },
+    "format" : { }
+  }
+  module.exports= department;
