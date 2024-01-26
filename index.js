123b18a70fb2f734145d1963f64ef83ac7f857aa
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-01-25 23:57:00 -0500

    error handeling complete

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ res.render('index')
 
 })
 
+app.use((error, req, res, next)=>{
+    error.statusCode = error.statusCode || 500;
+    error.status= error.status || 'error';
+    res.status(error.statusCode).json({
+        status: error.statusCode,
+        message:error.message
+    })
+})
 
 app.listen(port, ()=> console.log(`Server on port: ${port}`));
 
